6fb99a5caad0f1fd74e90fed4cb1b6222e33734d
Anshul <124551245+Anshul69@users.noreply.github.com>
2024-07-17 15:58:47 +0530

    Create 1.cpp

---
+++ 1.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+int arr[] = {1,2,3,4,5};
+cout<<"Elements of Array : \n";
+for(int i=0;i<5;i++){
+    cout<<arr[i]<<endl;
+}
+    return 0;
+}
